67184f9d3ac5bb20270b022064f94cadef604c74
Insu Hong <hinsu@rmsoft.kr>
2023-10-31 13:52:18 +0900

    Faker Table - Columns 설정

--- src/app/(main)/table/faker/components/columns.tsx
+++ src/app/(main)/table/faker/components/columns.tsx
@@ -2,3 +2,70 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { Person } from "../makeData";
+import { IndeterminateCheckbox } from "./checkbox";
+
+export const columns: ColumnDef<Person>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <IndeterminateCheckbox
+        {...{
+          checked: table.getIsAllRowsSelected(),
+          indeterminate: table.getIsSomeRowsSelected(),
+          onChange: table.getToggleAllRowsSelectedHandler(),
+        }}
+      />
+    ),
+    cell: ({ row }) => (
+      <div className="px-1">
+        <IndeterminateCheckbox
+          {...{
+            checked: row.getIsSelected(),
+            disabled: !row.getCanSelect(),
+            indeterminate: row.getIsSomeSelected(),
+            onChange: row.getToggleSelectedHandler(),
+          }}
+        />
+      </div>
+    ),
+  },
+  {
+    header: "Name",
+    footer: (props) => props.column.id,
+    columns: [
+      {
+        accessorKey: "firstName",
+        cell: (info) => info.getValue(),
+        footer: (props) => props.column.id,
+      },
+      {
+        accessorFn: (row) => row.lastName,
+        id: "lastName",
+        cell: (info) => info.getValue(),
+        header: () => <span>Last Name</span>,
+        footer: (props) => props.column.id,
+      },
+    ],
+  },
+  {
+    header: "Info",
+    footer: (props) => props.column.id,
+    columns: [
+      {
+        accessorKey: "visits",
+        header: () => <span>Visits</span>,
+        footer: (props) => props.column.id,
+      },
+      {
+        accessorKey: "status",
+        header: "Status",
+        footer: (props) => props.column.id,
+      },
+      {
+        accessorKey: "progress",
+        header: "Profile Progress",
+        footer: (props) => props.column.id,
+      },
+    ],
+  },
+];
