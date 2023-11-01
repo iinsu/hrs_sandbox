1b8d0814ad55e2027585b02cf6f7222e00e40943
Insu Hong <hinsu@rmsoft.kr>
2023-11-01 18:53:51 +0900

    Board - lexical 설치

--- src/app/(main)/board/page.tsx
+++ src/app/(main)/board/page.tsx
@@ -1,7 +1,67 @@
+"use client";
+
+import { $getRoot, $getSelection } from "lexical";
+import { useEffect, useState } from "react";
+
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+const theme = {
+  ltr: "ltr",
+  rtl: "rtl",
+  placeholder: "editor-placeholder",
+  paragraph: "editor-paragraph",
+};
+
+function MyCustomAutoFocusPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    // Focus the editor when the effect fires!
+    editor.focus();
+  }, [editor]);
+
+  return null;
+}
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don't throw them, Lexical will
+// try to recover gracefully without losing user data.
+function onError(error: any) {
+  console.error(error);
+}
+
 const BoardMainPage = () => {
+  const [editorState, setEditorState] = useState();
+
+  const initaialConfig = {
+    namespace: "MyEditor",
+    theme,
+    onError,
+  };
+
+  const onChange = (editorState: any) => {
+    const editorStateJSON = editorState.toJSON();
+    setEditorState(JSON.stringify(editorStateJSON) as any);
+  };
+
   return (
     <>
-      <div>Board Main Page</div>
+      <LexicalComposer initialConfig={initaialConfig}>
+        <PlainTextPlugin
+          contentEditable={<ContentEditable />}
+          placeholder={<div>Enter some text...</div>}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        <HistoryPlugin />
+        <MyCustomAutoFocusPlugin />
+        <OnChangePlugin onChange={onChange} />
+      </LexicalComposer>
     </>
   );
 };
