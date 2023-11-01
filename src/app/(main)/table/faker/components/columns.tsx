db72186e8fe555ada665452addfa3cf52a82bcf3
Insu Hong <hinsu@rmsoft.kr>
2023-11-01 10:54:03 +0900

    Faker Table - Column 분류 삭제

--- src/app/(main)/table/faker/components/columns.tsx
+++ src/app/(main)/table/faker/components/columns.tsx
@@ -30,52 +30,35 @@ export const columns: ColumnDef<Person>[] = [
     ),
   },
   {
-    header: "Name",
+    accessorKey: "firstName",
+    cell: (info) => info.getValue(),
     footer: (props) => props.column.id,
-    columns: [
-      {
-        accessorKey: "firstName",
-        cell: (info) => info.getValue(),
-        footer: (props) => props.column.id,
-      },
-      {
-        accessorFn: (row) => row.lastName,
-        id: "lastName",
-        cell: (info) => info.getValue(),
-        header: () => <span>Last Name</span>,
-        footer: (props) => props.column.id,
-      },
-    ],
   },
   {
-    header: "Info",
+    accessorFn: (row) => row.lastName,
+    id: "lastName",
+    cell: (info) => info.getValue(),
+    header: () => <span>Last Name</span>,
     footer: (props) => props.column.id,
-    columns: [
-      {
-        accessorKey: "age",
-        header: () => "Age",
-        footer: (props) => props.column.id,
-      },
-      {
-        header: "More Info",
-        columns: [
-          {
-            accessorKey: "visits",
-            header: () => <span>Visits</span>,
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorKey: "status",
-            header: "Status",
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorKey: "progress",
-            header: "Profile Progress",
-            footer: (props) => props.column.id,
-          },
-        ],
-      },
-    ],
+  },
+  {
+    accessorKey: "age",
+    header: () => "Age",
+    footer: (props) => props.column.id,
+  },
+  {
+    accessorKey: "visits",
+    header: () => <span>Visits</span>,
+    footer: (props) => props.column.id,
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    footer: (props) => props.column.id,
+  },
+  {
+    accessorKey: "progress",
+    header: "Profile Progress",
+    footer: (props) => props.column.id,
   },
 ];
