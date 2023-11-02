94c6cf5084ed7e4f6b40a69d4228fd07c01786c8
Insu Hong <hinsu@rmsoft.kr>
2023-11-02 15:52:01 +0900

    Lexical - placeholder 세팅

--- src/app/(main)/board/page.tsx
+++ src/app/(main)/board/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { $getRoot, $getSelection } from "lexical";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
@@ -14,7 +14,6 @@ import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 const theme = {
   ltr: "ltr",
   rtl: "rtl",
-  placeholder: "editor-placeholder",
   paragraph: "editor-paragraph",
 };
 
@@ -46,23 +45,38 @@ const BoardMainPage = () => {
   };
 
   const onChange = (editorState: any) => {
-    const editorStateJSON = editorState.toJSON();
-    setEditorState(JSON.stringify(editorStateJSON) as any);
+    /* const editorStateJSON = editorState.toJSON();
+    setEditorState(JSON.stringify(editorStateJSON) as any); */
+    editorState.read(() => {
+      const root = $getRoot();
+      const selection = $getSelection();
+      console.log(root, selection);
+    });
   };
 
+  const CustomContent = useMemo(() => {
+    return (
+      <ContentEditable className="ring-1 ring-black p-2 w-full rounded-md" />
+    );
+  }, []);
+
+  const CustomPlaceholder = useMemo(() => {
+    return <div className="absolute top-6 left-6">Enter some text...</div>;
+  }, []);
+
   return (
-    <>
+    <div className="w-[600px] relative p-4 bg-slate-100">
       <LexicalComposer initialConfig={initaialConfig}>
         <PlainTextPlugin
-          contentEditable={<ContentEditable />}
-          placeholder={<div>Enter some text...</div>}
+          contentEditable={CustomContent}
+          placeholder={CustomPlaceholder}
           ErrorBoundary={LexicalErrorBoundary}
         />
         <HistoryPlugin />
         <MyCustomAutoFocusPlugin />
         <OnChangePlugin onChange={onChange} />
       </LexicalComposer>
-    </>
+    </div>
   );
 };
 
