d8c486fe98fbed05fa4efff1891b8eccd7d97f79
iinsue <118152974+iinsu@users.noreply.github.com>
2023-11-09 17:19:40 +0900

    모달 만들기

---
+++ src/components/ui/modal.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import * as React from "react";
+import { ReactNode, useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+function ProtalImpl({
+  onClose,
+  children,
+  title,
+  closeOnClickOutside,
+}: {
+  children: ReactNode;
+  closeOnClickOutside: boolean;
+  onClose: () => void;
+  title: string;
+}) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (modalRef.current !== null) {
+      modalRef.current.focus();
+    }
+  }, []);
+
+  useEffect(() => {
+    let modalOverlayElement: HTMLElement | null = null;
+    const handler = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    const clickOutsideHandler = (event: MouseEvent) => {
+      const target = event.target;
+      if (
+        modalRef.current !== null &&
+        !modalRef.current.contains(target as Node) &&
+        closeOnClickOutside
+      ) {
+        onClose();
+      }
+    };
+
+    const modalElement = modalRef.current;
+    if (modalElement !== null) {
+      modalOverlayElement = modalElement.parentElement;
+      if (modalOverlayElement !== null) {
+        modalOverlayElement.addEventListener("click", clickOutsideHandler);
+      }
+    }
+
+    window.addEventListener("keydown", handler);
+
+    return () => {
+      window.removeEventListener("keydown", handler);
+      if (modalOverlayElement !== null) {
+        modalOverlayElement?.removeEventListener("click", clickOutsideHandler);
+      }
+    };
+  }, [closeOnClickOutside, onClose]);
+
+  return (
+    <div>
+      <div>
+        <h2>{title}</h2>
+        <button onClick={onClose}>X</button>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default function Modal({
+  onClose,
+  children,
+  title,
+  closeOnClickOutside = false,
+}: {
+  children: ReactNode;
+  closeOnClickOutside?: boolean;
+  onClose: () => void;
+  title: string;
+}): JSX.Element {
+  return createPortal(
+    <ProtalImpl
+      onClose={onClose}
+      title={title}
+      closeOnClickOutside={closeOnClickOutside}
+    >
+      {children}
+    </ProtalImpl>,
+    document.body
+  );
+}
