a9b776b21afef30613b0f1a76d3201a86aee6d6d
Insu Hong <118152974+iinsu@users.noreply.github.com>
2023-11-27 12:27:24 +0900

    Lexical - Rich Editor Page 추가

--- src/app/(main)/board/page.tsx
+++ src/app/(main)/board/page.tsx
@@ -3,13 +3,18 @@ import Link from "next/link";
 
 const BoardMainPage = () => {
   return (
-    <div className="bg-slate-200 h-full pt-11 flex flex-col gap-2 items-center">
-      <Link href="/board/plain">
-        <Button>Lexical Plain Text Editor</Button>
-      </Link>
-      <Link href="/board/notice">
-        <Button>Lexical Rich Text Editor</Button>
-      </Link>
+    <div className="bg-slate-200 h-full pt-11 ">
+      <div className="flex flex-col gap-2 items-center">
+        <Link href="/board/plain">
+          <Button>Lexical Plain Text Editor</Button>
+        </Link>
+        <Link href="/board/rich">
+          <Button>Lexical Rich Text Editor</Button>
+        </Link>
+        <Link href="/board/notice">
+          <Button>Custom Lexical Editor</Button>
+        </Link>
+      </div>
     </div>
   );
 };
