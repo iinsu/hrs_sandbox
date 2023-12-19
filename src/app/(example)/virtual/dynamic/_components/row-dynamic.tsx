8753a87ea6b263a48eb4306cc2edebc2b62867fc
Insu Hong <hinsu@rmsoft.kr>
2023-12-19 13:02:25 +0900

    Virtual - Dynamic Row Example

--- src/app/(example)/virtual/dynamic/_components/row-dynamic.tsx
+++ src/app/(example)/virtual/dynamic/_components/row-dynamic.tsx
@@ -48,7 +48,40 @@ export const RowVirtualDynamic = () => {
           </Button>
         </div>
         <hr />
-        <div ref={parentRef}></div>
+        <div
+          ref={parentRef}
+          className="border max-w-full h-[400px] w-[400px] overflow-y-auto"
+          style={{
+            contain: "strict",
+          }}
+        >
+          <div
+            className="w-full relative"
+            style={{
+              height: virtualizer.getTotalSize(),
+            }}
+          >
+            <div
+              className="absolute top-0 left-0 w-full"
+              style={{
+                transform: `translateY(${items[0]?.start ?? 0}px)`,
+              }}
+            >
+              {items.map((virtualRow) => (
+                <div
+                  key={virtualRow.key}
+                  ref={virtualizer.measureElement}
+                  data-index={virtualRow.index}
+                >
+                  <div className="py-2">
+                    <div>Row {virtualRow.index}</div>
+                    <div>{sentences[virtualRow.index]}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
