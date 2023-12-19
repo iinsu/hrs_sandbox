0d21532f5660724f26a5aa1d50dff40096e7fe9f
Insu Hong <hinsu@rmsoft.kr>
2023-12-19 11:14:06 +0900

    Virtual - Dynamic 예제 페이지 만들기

--- src/app/(example)/virtual/page.tsx
+++ src/app/(example)/virtual/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const VirtualMainPage = () => {
   return (
     <>
-      <div>Virtual Main Page</div>
+      <div className="h-full bg-slate-50 w-full flex justify-center pt-16">
+        <Link href="/virtual/dynamic">
+          <Button>Dynamic Example</Button>
+        </Link>
+      </div>
     </>
   );
 };
